5ecb14be54131ecda8a80f92cb8a8eec1b95d2d6
Chun Han Lu <chunhanlu@Chuns-MacBook-Pro.local>
2021-05-30 16:58:22 -0500

    move

--- cmd/run/main.py
+++ cmd/run/main.py
@@ -1,35 +1,8 @@
-import sys
 import cbpro
-import time
-import json
+from cmd.run.secret import API_KEY
+from cmd.run.secret import PASS_PHRASE
+from cmd.run.secret import B64_SECRET
 
-class MyWebsocketClient(cbpro.WebsocketClient):
-    def on_open(self):
-        self.url = "wss://ws-feed.pro.coinbase.com/"
-        self.products = ["BTC-USD", "ETH-USD"]
-        self.message_count = 0
-        print("Let's count the messages!")
 
-    def on_message(self, msg):
-        print(json.dumps(msg, indent=4, sort_keys=True))
-        self.message_count += 1
-
-    def on_close(self):
-        print("-- Goodbye! --")
-
-# Channel options: ['ticker', 'user', 'matches', 'level2', 'full']
-
-wsClient = MyWebsocketClient(auth=True, channels=None)
-wsClient.start()
-print(wsClient.url, wsClient.products)
-try:
-    while True:
-        print("\nMessageCount =", "%i \n" % wsClient.message_count)
-        time.sleep(1)
-except KeyboardInterrupt:
-    wsClient.close()
-
-if wsClient.error:
-    sys.exit(1)
-else:
-    sys.exit(0)
+auth_client = cbpro.AuthenticatedClient(API_KEY, B64_SECRET, PASS_PHRASE)
+print(auth_client.get_accounts())
